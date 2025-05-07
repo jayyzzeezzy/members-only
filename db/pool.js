2a5bc6c63c1da70f0927a85e725967360841d65b
James Zhou <jam9es@gmail.com>
2025-05-06 23:12:26 -0700

    Add to project skeleton with node-postgres package

---
+++ db/pool.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+const { argv } = require("node:process");
+const { Pool } = require("pg");
+
+// establish local or remote db here
+const dbConnection = argv[2];
+
+module.exports = new Pool({
+    connectionString: process.env[`${dbConnection}`]
+});
