814e4ac009950dc771d93671b01894016c0ea534
James Zhou <jam9es@gmail.com>
2025-05-10 17:21:06 -0700

    Refactor connectionString in pool.js

--- db/pool.js
+++ db/pool.js
@@ -1,10 +1,6 @@
 require("dotenv").config();
-const { argv } = require("node:process");
 const { Pool } = require("pg");
 
-// establish local or remote db here
-const dbConnection = argv[2];
-
 module.exports = new Pool({
-    connectionString: process.env[`${argv[2]}`]
+    connectionString: process.env.DATABASE_URL
 });
